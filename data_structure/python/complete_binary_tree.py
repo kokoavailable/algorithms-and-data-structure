a14e7abb8d73ff67ad48f2c2fa3144bb6aa6d100
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-10 17:38:11 +0900

    Update complete_binary_tree.py

--- data_structure/python/complete_binary_tree.py
+++ data_structure/python/complete_binary_tree.py
@@ -4,8 +4,26 @@ class CompleteBinaryTree:
 
   def insert(self, key):
     self.tree.append(key)
-    self.heapify_up(len(self.tree) -1)
-
-  def heapify_up(self, index):
-    parent_index = (index - 1) // 2
     
+  def display_tree(self):
+    if not self.tree:
+        print("Tree is empty")
+        return
+
+    depth = 0
+    while 2 ** depth <= len(self.tree):
+        depth += 1
+    
+    index = 0
+    for level in range(depth):
+        level_length = 2 ** level
+        for _ in range(level_length):
+            if index < len(self.tree):
+                print(self.tree[index], end=' ')
+                index += 1
+        print()
+
+
+# 50
+# 30 20
+# 40 70 60
