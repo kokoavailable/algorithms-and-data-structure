5e42911082423970ee86dace05b49789d795f50d
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-11 12:29:21 +0900

    Time: 32 ms (81.09%), Space: 12.1 MB (64.19%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -6,27 +6,27 @@ class Solution(object):
         :type senate: str
         :rtype: str
         """
-        dq = deque((i, c) for i, c in enumerate(senate))
+        radiant = deque()
+        dire = deque()
         
-        while dq:
-            i, c = dq.popleft()
-            if c == 'R':
-                for j in range(len(dq)):
-                    if dq[j][1] == 'D':
-                        dq.remove(dq[j])
-                        break
-                dq.append((i + len(senate), 'R'))
+        for i, s in enumerate(senate):
+            if s == 'R':
+                radiant.append(i)
             else:
-                for j in range(len(dq)):
-                    if dq[j][1] == 'R':
-                        dq.remove(dq[j])
-                        break
-                dq.append((i + len(senate), 'D'))
+                dire.append(i)
                 
-            if not any(x[1] == 'R' for x in dq):
-                return "Dire"
-            if not any(x[1] == 'D' for x in dq):
-                return "Radiant"
+        n = len(senate)
+                
+        while radiant and dire:
+            r_index = radiant.popleft()
+            d_index = dire.popleft()
+            
+            if r_index < d_index:
+                radiant.append(r_index + n)
+            else:
+                dire.append(d_index + n)
+                
+        return "Radiant" if radiant else "Dire"
         
         # 선공권자는 뒷사람 죽인다. 그리고 뒤에로 다시 어펜드 된다. 최후의 1인이 남는다면, 그 소속당을 승리당으로 선언한다.
         
