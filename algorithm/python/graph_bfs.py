4c73d30ad9b9f73beffaf76976daefaf9b4cbfc3
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-15 00:12:18 +0900

    Update graph_bfs.py

--- algorithm/python/graph_bfs.py
+++ algorithm/python/graph_bfs.py
@@ -1,7 +1,11 @@
 from collections import deque
 
 def bfs(graph,start):
-  visited = set() 
+  """
+  그래프에서 레벨이란 루트로드로부터 해당 노드까지의 간선의 수.
+  트리의 bfs와 비슷하게, 같은 레벨부터 순차적으로 탐색한다.
+  """
+  visited = set() # set을 사용하면 중복 처리가 필요가 없고 요소의 존재 여부를 확인하는 연산이 O(1)의 시간 복잡도를 가진다.
   queue = dequeue([start])
   result = []
 
