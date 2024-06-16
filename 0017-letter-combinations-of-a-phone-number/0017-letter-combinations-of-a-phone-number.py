4fe2fe83890a39256f3b284bb4e1719f4739b05c
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-17 03:06:07 +0900

    Time: 12 ms (74.24%), Space: 11.7 MB (30.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -4,35 +4,56 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
-        # 인덱스에 따라서 해당 문자저장해놓고, 세개씩 보여주면 되잖아 . 조합으로. 근데 이건 브루트 포스인데
+#         # 인덱스에 따라서 해당 문자저장해놓고, 세개씩 보여주면 되잖아 . 조합으로. 근데 이건 브루트 포스인데
         
+#         if not digits:
+#             return []
+        
+#         # 숫자에 대응되는 문자 매핑 리스트
+#         alphabet_list = [
+#             [], [], ['a', 'b', 'c'], ['d', 'e', 'f'], 
+#             ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], 
+#             ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']
+#         ]
+        
+#         # 초기 결과 리스트
+#         result = [""]
+        
+#         # 각 숫자에 대해 반복
+#         for digit in digits:
+#             number = int(digit)
+#             new_result = []
+            
+#             # 현재 숫자에 해당하는 문자들을 가져옴
+#             possible_chars = alphabet_list[number]
+            
+#             # 기존 결과에 새 문자 추가
+#             for combination in result:
+#                 for char in possible_chars:
+#                     new_result.append(combination + char)
+            
+#             # 결과 업데이트
+#             result = new_result
+        
+    
         if not digits:
             return []
         
-        # 숫자에 대응되는 문자 매핑 리스트
-        alphabet_list = [
-            [], [], ['a', 'b', 'c'], ['d', 'e', 'f'], 
-            ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], 
-            ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']
-        ]
+        phone_map = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
         
-        # 초기 결과 리스트
-        result = [""]
+        result = []
         
-        # 각 숫자에 대해 반복
-        for digit in digits:
-            number = int(digit)
-            new_result = []
-            
-            # 현재 숫자에 해당하는 문자들을 가져옴
-            possible_chars = alphabet_list[number]
-            
-            # 기존 결과에 새 문자 추가
-            for combination in result:
-                for char in possible_chars:
-                    new_result.append(combination + char)
-            
-            # 결과 업데이트
-            result = new_result
+        def backtrack(combination, next_digits):
+            if len(next_digits) == 0:
+                result.append(combination)
+            else:
+                for letter in phone_map[next_digits[0]]:
+                    backtrack(combination + letter, next_digits[1:])
+                    
+        result = []
+        backtrack("", digits)
         
         return result
