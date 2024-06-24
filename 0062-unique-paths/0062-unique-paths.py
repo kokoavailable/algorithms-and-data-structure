5cb3289009c37488dbe708e28f23ffbb20550d90
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-24 17:07:47 +0900

    Time: 12 ms (76.97%), Space: 11.6 MB (67.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,10 +5,15 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[1] * n for _ in range(m)]
+        dp = [[0] * n for _ in range(m)]
         
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-                
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+        
         return dp[m-1][n-1]
