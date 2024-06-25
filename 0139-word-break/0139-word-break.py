49aeb2f1c57814bcc4fdfec606310de3eec29499
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-25 18:29:23 +0900

    Time: 22 ms (47.13%), Space: 11.9 MB (22.53%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -15,7 +15,7 @@ class Solution(object):
         
         for i in range(1, len(s) + 1):
             for j in range(i):
-                if dp[j] and s[j:i] in word_set:
+                if dp[j] == True and s[j:i] in word_set:
                     dp[i] = True
                     break
                     
