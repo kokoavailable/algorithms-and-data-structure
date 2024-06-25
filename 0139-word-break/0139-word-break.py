c7f65c230e1d6ce6ee02c8982c59d3237e0ff773
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-25 18:25:44 +0900

    Time: 25 ms (30.22%), Space: 11.9 MB (22.53%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        # dictionary words 를 재사용할 수 있다.. 
+        # 가능한 조합들을 다 생각해봐야할 것 같은데..
+        # 일단 브루트포스를 생각해보자.
+        word_set = set(wordDict)
+        
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+                    
+        return dp[len(s)]
