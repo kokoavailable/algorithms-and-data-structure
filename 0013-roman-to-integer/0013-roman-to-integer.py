24d48add6a4409cc4a8212aa2e7ced5175ee885a
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-26 15:35:27 +0900

    Time: 30 ms (56.72%), Space: 11.7 MB (43.85%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,48 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        answer = 0
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
         
-        for i in range(len(s) - 1):
-            if s[i] == 'C' and s[i+1] in ["D", "M"]:
-                answer -= 100
-                continue
-            elif s[i] == 'X' and s[i+1] in ["L", "C"]:
-                answer -= 10
-                continue
-            elif s[i] == 'I' and s[i+1] in ["V", "X"]:
-                answer -= 1
-                continue
-            
-            if s[i] == "I":
-                answer += 1
-            elif s[i] == "V":
-                answer += 5
-            elif s[i] == "X":
-                answer += 10
-            elif s[i] == "L":
-                answer += 50
-            elif s[i] == "C":
-                answer += 100
-            elif s[i] == "D":
-                answer += 500
-            elif s[i] == "M":
-                answer += 1000
-                
-            
-        if s[-1] == "I":
-            answer += 1
-        elif s[-1] == "V":
-            answer += 5
-        elif s[-1] == "X":
-            answer += 10
-        elif s[-1] == "L":
-            answer += 50
-        elif s[-1] == "C":
-            answer += 100
-        elif s[-1] == "D":
-            answer += 500
-        elif s[-1] == "M":
-            answer += 1000
+        answer = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i+1]]:
+                answer -= roman_to_int[s[i]]
+            else:
+                answer += roman_to_int[s[i]]
             
         return answer 
