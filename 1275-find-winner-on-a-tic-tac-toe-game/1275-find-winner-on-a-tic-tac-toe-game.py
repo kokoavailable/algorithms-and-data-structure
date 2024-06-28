257da8351cd882c8fffc263856694453f961ff31
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-06-28 14:19:52 +0900

    Time: 16 ms (60.19%), Space: 11.5 MB (66.20%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -6,16 +6,9 @@ class Solution(object):
         """
         result_grid = [[""] * 3 for _ in range(3)]
         
-        def placing_characters(moves):
-            turn_flag = 1
-            for move in moves:
-                x = move[0]
-                y = move[1]
-                if turn_flag % 2 != 0:    
-                    result_grid[x][y] = "X" # turn A
-                else:
-                    result_grid[x][y] = "O" # turn B
-                turn_flag += 1
+        for i, move in enumerate(moves):
+            x, y = move
+            result_grid[x][y] = "X" if i % 2 == 0 else "O"
             
         def check_characters(result_grid):
             for i in range(3):
@@ -29,8 +22,7 @@ class Solution(object):
             if result_grid[0][2] == result_grid[1][1] == result_grid[2][0] != "":
                 return result_grid[0][2]
             return None
-        
-        placing_characters(moves)
+    
         winner = check_characters(result_grid)
 
         if winner == "X":
