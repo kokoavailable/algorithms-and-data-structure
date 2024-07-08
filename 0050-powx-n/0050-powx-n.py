e60421bc8b8751b7e67b4cf4c46fe7a04213f45c
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-07-08 11:21:45 +0900

    Time: 14 ms (58.93%), Space: 11.7 MB (8.22%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,4 +5,24 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        return x ** n
+        
+        if n == 0:
+            return 1.0
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+            
+        def power(x, n):
+            if n == 0:
+                if n == 0:
+                    return 1.0
+            half = power(x, n // 2)
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+            
+        return power(x, n)
+        
+        
