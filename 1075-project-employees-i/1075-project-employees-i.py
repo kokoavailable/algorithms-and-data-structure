8427c47baa7940ceaa9209a045ad0151a4a3d4f4
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-02 00:35:30 +0900

    Time: 404 ms (50.30%), Space: 71.1 MB (76.96%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def project_employees_i(project: pd.DataFrame, employee: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(project, employee, on="employee_id")
+    
+    result = (
+        merged_df.groupby("project_id")["experience_years"]
+        .mean()
+        .round(2)
+        .reset_index()
+        .rename(columns={"experience_years": "average_years"})
+    )
+
+    return result
+
+    merged_df = pd.merge(project, employee, on="employee_id")
+    
+    result = (
+        merged_df.groupby("project_id")["experience_years"]
+        .means()
+        .round(2)
+        .reset_index()
+        .rename(columns={"experience_years": "average_years"})
+    )
