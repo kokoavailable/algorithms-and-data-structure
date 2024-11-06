9c48e6a9fca3717c2afa38777fb7f2e2bbf830ca
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-07 03:53:33 +0900

    Time: 531 ms (64.00%), Space: 72.2 MB (10.11%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.py
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.py
@@ -1,23 +1,18 @@
 import pandas as pd
+import numpy as np
 
 def count_employees(employees: pd.DataFrame) -> pd.DataFrame:
     reports = employees[~employees['reports_to'].isna()]
     
-    # 상사별로 그룹화하고 직원 수와 평균 나이를 계산
     result = reports.groupby('reports_to').agg(
         reports_count=('employee_id', 'size'),
         average_age=('age', 'mean')
     ).reset_index()
     
-    # 평균 나이를 정수로 반올림하여 변경
-    result['average_age'] = (result['average_age'] + 0.5).astype(int)
+    result['average_age'] = np.floor(result['average_age'] + 0.5).astype(int)
     
-    print(result)
-    
-    # 상사 이름과 employee_id를 원래 테이블에서 가져옴
     result = result.merge(employees[['employee_id', 'name']], left_on='reports_to', right_on='employee_id')
     
-    # 컬럼 이름 재정의 및 정렬
     result = result[['employee_id', 'name', 'reports_count', 'average_age']].sort_values('employee_id')
     
     return result
