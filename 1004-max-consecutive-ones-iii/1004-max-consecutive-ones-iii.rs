8a7720bddebe4a6cf46b4c57e02854f9ebd19305
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-15 13:44:47 +0900

    Time: 0 ms (100.00%), Space: 2.4 MB (59.68%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.rs
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.rs
@@ -1,19 +1,35 @@
 impl Solution {
     pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
-        let (mut i, mut max_len, mut zero_cnt) = (0, 0, 0);
-        for j in 0..nums.len() {
-            if nums[j] == 0 {
-                zero_cnt += 1;
-            }
-        
-            while zero_cnt > k {
-                if nums[i as usize] == 0 {
-                    zero_cnt -= 1;
+        let(mut i, mut zero_cnt, mut max_len) = (0, 0, 0);
+            for j in 0..nums.len() {
+                if nums[j] == 0{
+                    zero_cnt += 1;
                 }
-                i += 1;
+            
+                while zero_cnt > k {
+                   if nums[i as usize] == 0 {
+                       zero_cnt -= 1;
+                    }
+                    i += 1;
+                }
+                max_len = max_len.max((j as i32) - i + 1);
+                
             }
-            max_len = max_len.max((j as i32) - i + 1);
-        }
-        max_len
+            max_len
+//         let (mut i, mut max_len, mut zero_cnt) = (0, 0, 0);
+//         for j in 0..nums.len() {
+//             if nums[j] == 0 {
+//                 zero_cnt += 1;
+//             }
+        
+//             while zero_cnt > k {
+//                 if nums[i as usize] == 0 {
+//                     zero_cnt -= 1;
+//                 }
+//                 i += 1;
+//             }
+//             max_len = max_len.max((j as i32) - i + 1);
+//         }
+//         max_len
     }
 }
