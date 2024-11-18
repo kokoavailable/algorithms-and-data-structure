fd5dd7cb630cef9c45d787bee08ca89ff71253ec
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-18 17:09:25 +0900

    Time: 516 ms (84.02%), Space: 0B (100.00%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,21 @@
+-- Write your PostgreSQL query statement below
+
+WITH 
+    a2 AS (
+        SELECT
+            player_id,
+            MIN(event_date) AS min_date
+        FROM Activity
+        GROUP BY player_id
+    )
+SELECT ROUND(
+            COUNT(a1.player_id)::numeric / (SELECT COUNT(player_id) FROM a2),
+            2
+        ) AS fraction
+FROM 
+    Activity a1
+JOIN 
+    a2
+ON 
+    a1.player_id = a2.player_id
+    AND a2.min_date = a1.event_date - INTERVAL '1 DAY';
