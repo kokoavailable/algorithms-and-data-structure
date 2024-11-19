b8dc17601df8bd605c0e8c637de2ba458e80171f
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-19 14:47:05 +0900

    Time: 426 ms (67.42%), Space: 73.5 MB (11.13%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.py
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.py
@@ -4,14 +4,10 @@ def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
     first_year = sales.groupby('product_id')['year'].min().reset_index()
     first_year.rename(columns={'year':'first_year'}, inplace=True)
     
-    print(first_year)
-    
     merged = first_year.merge(sales, on='product_id')
+    
     filtered = merged[merged['year'] == merged['first_year']]
     
-    result = filtered.drop(columns=['year'])
-    neworder = ['product_id', 'first_year', 'quantity', 'price']
+    result = filtered[['product_id', 'first_year', 'quantity', 'price']]
     
-    result_reordered = result[neworder]
-    
-    return result_reordered
+    return result
