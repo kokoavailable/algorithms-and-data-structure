0a9b8ad08d3b786137989d5062011b76c63f25ce
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-20 18:18:42 +0900

    Time: 0 ms (100.00%), Space: 16.6 MB (52.44%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -10,10 +10,10 @@ class Solution:
             if not t1 and not t2:
                 return True
 
-            if not t1 or not t2:
+            if not t1 or not t2 or t1.val != t2.val:
                 return False
 
-            return (t1.val == t2.val and isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left))
+            return isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
         return isMirror(root, root)
             
             
