cacff0193770aea8f9be476ed66ec346575b485f
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-11-29 15:11:36 +0900

    Time: 11 ms (9.54%), Space: 17.4 MB (10.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,23 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        print(l1, l2)
-        l3 = ListNode(None)
         carry = 0
         def listnode(check1, check2, carry):
             if not check1 and not check2 and carry == 0:
-                return None  # 재귀 종료 시 None 반환
+                return None
 
-            # 현재 자리 계산
             val1 = check1.val if check1 else 0
             val2 = check2.val if check2 else 0
             total = val1 + val2 + carry
 
-            # 현재 노드 생성
             carry = total // 10
             current_node = ListNode(total % 10)
 
-            # 다음 노드로 재귀 호출
             next1 = check1.next if check1 else None
             next2 = check2.next if check2 else None
             current_node.next = listnode(next1, next2, carry)
