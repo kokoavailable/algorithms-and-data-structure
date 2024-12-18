31724e12d549b95c00daf5773001149c51bfee98
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2024-12-18 19:25:30 +0900

    Time: 49 ms (79.12%), Space: 49.1 MB (38.93%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -5,12 +5,10 @@
  */
 var filter = function(arr, fn) {
     const filteredArr = [];
-    
     for (let i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
             filteredArr.push(arr[i]);
         }
     }
-    
     return filteredArr;
 };
