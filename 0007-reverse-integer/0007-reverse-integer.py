743da1fa9ecfd2c63e82dab1da5766e26703e881
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2025-03-16 17:59:21 +0900

    Time: 42 ms (38.62%), Space: 17.7 MB (83.04%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,6 +4,34 @@ class Solution:
         x_abs = abs(x)
         max_limit = 2**31 - 1 if sign == 1 else 2**31
         reversed_num = 0
+
+        while x_abs > 0:
+            pop = x_abs % 10
+            x_abs = x_abs // 10
+
+            if reversed_num > max_limit // 10 or (reversed_num == max_limit // 10 and pop > max_limit % 10):
+                return 0
+
+            reversed_num = reversed_num * 10 + pop
+
+        
+        return reversed_num * sign
+
+
+
+
+
+
+
+
+
+
+
+
+        sign = 1 if x >= 0 else -1
+        x_abs = abs(x)
+        max_limit = 2**31 - 1 if sign == 1 else 2**31
+        reversed_num = 0
         
         while x_abs > 0:
             pop = x_abs % 10
