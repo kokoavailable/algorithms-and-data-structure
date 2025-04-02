ab7d1e4677e1fc8b3fe865526b6c34ec23771d65
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2025-04-03 01:25:37 +0900

    Time: 2 ms (79.58%), Space: 17.9 MB (19.91%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,7 +1,7 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         shift = 0
-        while left < right:
+        while left != right:
             left >>= 1
             right >>= 1
             shift += 1
