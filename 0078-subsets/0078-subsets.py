faf4ab8e0ecbe5ce98fa2baf68087ca074814ef4
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2025-04-14 19:47:00 +0900

    Time: 0 ms (100%), Space: 18 MB (21.44%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,7 +1,9 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-        for i in range(len(nums) + 1):  # 0부터 n까지 조합
+        for i in range(len(nums) + 1): 
             for combo in combinations(nums, i):
                 result.append(list(combo))
         return result
+
+
