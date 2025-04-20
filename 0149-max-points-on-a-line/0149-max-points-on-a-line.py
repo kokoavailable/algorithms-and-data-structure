fa40adb915e60bf945c5d2dcdc059b623506d161
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2025-04-20 18:56:21 +0900

    Time: 15 ms (98.54%), Space: 17.9 MB (73.72%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -2,15 +2,17 @@ class Solution:
     def maxPoints(self, points: list[list[int]]) -> int:
         points.sort()
         slope, M = defaultdict(int), 0
-        
+
         for i, (x1, y1) in enumerate(points):
             slope.clear()
 
             for x2, y2 in points[i + 1:]:
                 dx, dy = x2 - x1, y2 - y1
 
-                G = gcd(dx, dy)                 
-                m = (dx//G,dy//G)
+                if dx == 0:
+                    m = 'inf' 
+                else:
+                    m = dy / dx                 
                 
                 slope[m] += 1
                 if slope[m] > M: M = slope[m]
