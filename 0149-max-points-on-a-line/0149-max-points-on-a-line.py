bbf9bf32779b0fa69fa5bb6a1af2e1bb2f869b72
Paul Bae <165033012+kokoavailable@users.noreply.github.com>
2025-04-20 17:22:16 +0900

    Time: 27 ms (77.5%), Space: 17.9 MB (73.72%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,44 +1,20 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
-        if n <= 2:
-            return n
-
-        max_points = 0
-
-        for i in range(n):
-            slope_count = defaultdict(int)
-            same_points = 1
-            curr_max = 0
-
-            x1, y1 = points[i]
-
-            for j in range(i + 1, n):
-                x2, y2 = points[j]
-
-                dx = x2 - x1
-                dy = y2 - y1
-
-                if dx == 0 and dy == 0:
-                    same_points += 1
-                    continue
-
-                if dx == 0:
-                    slope = ('inf', 0)
-                else:
-                    g = gcd(dy, dx)
-                    dy //= g
-                    dx //= g
-
-                    if dx < 0:
-                        dy *= -1
-                        dx *= -1
-
-                    slope = (dy, dx)
-
-                slope_count[slope] += 1
-                curr_max = max(curr_max, slope_count[slope])
-
-            max_points = max(max_points, curr_max + same_points)
-
-        return max_points
+        if len(points) <= 2:
+            return len(points)
+        
+        def find_slope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+            if x1-x2 == 0:
+                return inf
+            return (y1-y2)/(x1-x2)
+        
+        ans = 1
+        for i, p1 in enumerate(points):
+            slopes = defaultdict(int)
+            for j, p2 in enumerate(points[i+1:]):
+                slope = find_slope(p1, p2)
+                slopes[slope] += 1
+                ans = max(slopes[slope], ans)
+        return ans+1
